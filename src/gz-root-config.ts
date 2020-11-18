e0b5368cd3c2ea8d8a8d38f46eee171f393c01bf
Carlos Filoteo <c.filoteo12@gmail.com>
2020-11-18 10:59:38 -0700

    Remove single-spa-layout

--- src/gz-root-config.ts
+++ src/gz-root-config.ts
@@ -1,47 +1,15 @@
 import { registerApplication, start } from "single-spa";
-import {
-  constructApplications,
-  constructRoutes,
-  constructLayoutEngine,
-} from "single-spa-layout";
-
-
-
-const routes = constructRoutes(
-  document.querySelector("#single-spa-layout") as HTMLTemplateElement
-);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
 
 registerApplication(
-  '@single-spa/welcome',
-  () => System.import('@single-spa/welcome'),
-  location => location.pathname === '/'
+  "@gz-/app1",
+  () => System.import("@gz-/app1"),
+  (location) => location.pathname.startsWith("/app1")
 );
 
 registerApplication(
-  '@gz-/app1',
-  () => System.import('@gz-/app1'),
-  location => location.pathname.startsWith('/app1')
+  "@gz/app2",
+  () => System.import("@gz/app2"),
+  (location) => location.pathname.startsWith("/app2")
 );
 
-registerApplication(
-  '@gz/app2',
-  () => System.import('@gz/app2'),
-  location => location.pathname.startsWith('/app2')
-);
-
-
-
-
-
-
-
-applications.forEach(registerApplication);
-layoutEngine.activate();
 start();
